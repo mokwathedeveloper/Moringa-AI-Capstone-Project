7d06744ee9aba979ca95226be81f9e33c3429063
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-02-19 16:59:31 +0300

    fix: add ChangeDetectorRef to resolve UI update issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 type QuoteResponse = {
   quote: string;
   author: string;
+  id: number;
 };
 
 @Component({
@@ -22,7 +23,7 @@ export class AppComponent implements OnInit {
 
   quote: { text: string; author: string } | null = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.fetchQuote();
@@ -32,17 +33,18 @@ export class AppComponent implements OnInit {
     this.loading = true;
     this.error = null;
 
-    // Public quotes API (reliable fallback)
     const url = 'https://dummyjson.com/quotes/random';
 
     this.http.get<QuoteResponse>(url).subscribe({
       next: (res) => {
         this.quote = { text: res.quote, author: res.author };
         this.loading = false;
+        this.cdr.detectChanges();
       },
       error: () => {
         this.error = 'Could not load a quote right now. Please try again.';
         this.loading = false;
+        this.cdr.detectChanges();
       },
     });
   }
